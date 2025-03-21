607a7f32074b3ae35ef7e441cfcb38415d6e5176
SEOJINWOONG <tjwlsdnd12@gmail.com>
2025-03-21 18:31:32 +0900

    first commit

---
+++ class/section01/01-02-token-api/index.js
@@ -0,0 +1,40 @@
+// 안좋은 예
+// function createTokenOfPhone(qqq) { // 매개변수(parameter)
+//   // * 1. 휴대폰번호 자릿수 맞는지 확인하기(10~11자리)
+//   if(qqq.length >= 10 ) {
+//     if (qqq.length <= 11) {
+//       // * 2. 핸드폰 토큰 6자리 만들기
+//       const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+//       console.log("🚀 ~ createTokenOfPhone ~ result:", result)
+//       // * 3. 핸드폰번호에 토큰 전송하기
+
+//       console.log(qqq + '번호로 인증번호 ' + result + '를 전송합니다.');
+      
+//     } else {
+//       console.log("에러발생! 핸드폰 번호를 제대로 입력해주세요");
+//     }
+//   } else {
+//     console.log("에러발생! 핸드폰 번호를 제대로 입력해주세요");
+//   }
+// }
+
+
+
+
+
+// 좋은 예
+function createTokenOfPhone(qqq) { // 매개변수(parameter)
+  // * 1. 휴대폰번호 자릿수 맞는지 확인하기(10~11자리)
+  if (qqq.length < 10 || qqq.length > 11) {
+    console.log("에러발생! 핸드폰 번호를 제대로 입력해주세요");
+    return;    
+  }
+
+  // * 2. 핸드폰 토큰 6자리 만들기
+  const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  // * 3. 핸드폰번호에 토큰 전송하기
+
+  console.log(qqq + '번호로 인증번호 ' + result + '를 전송합니다.');
+}
+
+createTokenOfPhone("01012345678"); // 인자(argument)
